Fix standard deviation calculation to use predictions only in logging

trainer.py
@@ -27,7 +27,7 @@ class LogTrainer(Trainer):
                 logs[f"{prefix}r2"] = compute_r2(self.epoch_wise_predictions, self.epoch_wise_labels)
                 logs[f"{prefix}pearson"] = float(pearsonr(self.epoch_wise_predictions, self.epoch_wise_labels)[0])
                 logs[f"{prefix}mae"] = compute_mae(self.epoch_wise_predictions, self.epoch_wise_labels)
-                logs[f"{prefix}std"] = compute_std(self.epoch_wise_predictions, self.epoch_wise_labels)
+                logs[f"{prefix}std"] = compute_std(self.epoch_wise_predictions)
                 logs[f"{prefix}mse"] = compute_mse(self.epoch_wise_predictions, self.epoch_wise_labels)
                 self.epoch_wise_predictions = torch.tensor([])
                 self.epoch_wise_labels = torch.tensor([])

utils.py
@@ -38,8 +38,9 @@ def preprocess_example(example, image_processor, num_frames=32):
 def compute_mae(predictions, labels):
     return torch.mean(torch.abs(predictions - labels)).item()
 
-def compute_std(predictions, labels):
-    return torch.std(predictions - labels).item()
+# TODO what are we doing here...
+def compute_std(predictions):
+    return torch.std(predictions).item()
 
 def compute_mse(predictions, labels):
     return torch.mean((predictions - labels) ** 2).item()
@@ -66,7 +67,7 @@ def compute_metrics(eval_pred):
 
     return {
         "mae_e": compute_mae(predictions, labels),
-        "std_e": compute_std(predictions, labels),
+        "std_e": compute_std(predictions),
         "mse_e": compute_mse(predictions, labels),
         "r2_e": compute_r2(predictions, labels),
         "pearson_e": float(pearson_corr),